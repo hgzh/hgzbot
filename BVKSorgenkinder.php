f62bd9dd5c23e4caf438a5edb88b627196c1071d
hgzh <konradfl@outlook.com>
2019-02-01 18:10:36 +0100

    BVKSorgenkinder: fetch empty results and regex fix

--- BVKSorgenkinder.php
+++ BVKSorgenkinder.php
@@ -19,9 +19,15 @@
 		}
 		
 		protected function run() {
-						
+			
 			$json = $this->mw->toolPetscanPost('psid=6275628&format=json');
 			$pages = $json['*'][0]['a']['*'];
+			
+			if (count($pages) == 0) {
+				$this->mw->log('empty petscan response: ' . serialize($json));
+				$this->mw->logoutUser();
+				return;
+			}
 			
 			$output = '';
 			$i      = 0;
@@ -32,7 +38,7 @@
 			}
 			
 			$newtext = $this->mw->getWikitext('Wikipedia:WikiProjekt Bundesverdienstkreuz/Sorgenkinder');
-			$newtext = preg_replace('/<!--\s*HGZ_BVK_START\s*-->(.+)<!--\s*HGZ_BVK_END\s*-->/s', '<!-- HGZ_BVK_START -->' . $output . '<!-- HGZ_BVK_END -->', $newtext, 1);
+			$newtext = preg_replace('/<!--\s*HGZ_BVK_START\s*-->(.*)<!--\s*HGZ_BVK_END\s*-->/s', '<!-- HGZ_BVK_START -->' . $output . '<!-- HGZ_BVK_END -->', $newtext, 1);
 			
 			$this->mw->editPage('Wikipedia:WikiProjekt Bundesverdienstkreuz/Sorgenkinder', $newtext, 'Bot: Aktualisiere Liste (' . $i . ' Einträge)');
 			
